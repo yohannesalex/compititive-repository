5e1bc2c6ef848ecd2232a9b6d97a7e94db6c0106
yohannesalex <133195614+yohannesalex@users.noreply.github.com>
2023-12-05 10:51:53 +0300

    Added solution for Keyboard Row

---
+++ week1/keyboard-row.py
@@ -0,0 +1,25 @@
+class Solution:
+    def findWords(self, words: List[str]) -> List[str]:
+        top="qwertyuiop"
+        middle="asdfghjkl"
+        bottom = "zxcvbnm"
+        res=[]
+        for i in words:
+            t=0
+            m=0
+            b=0
+            for j in i:
+                if j.lower() in top:
+                    t+=1
+                elif j.lower() in middle:
+                    m+=1
+                elif j.lower() in bottom:
+                    b+=1
+            if len(i) == t:
+                res.append(i)
+            elif len(i) == m:
+                res.append(i)
+            elif len(i) == b:
+                res.append(i)
+        return res
+
