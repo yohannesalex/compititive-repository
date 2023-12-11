fc33755636ac012b008784658b090fd4f96f45a8
yohannesalex <133195614+yohannesalex@users.noreply.github.com>
2023-12-11 19:31:25 +0300

    Added solution for Majority Element

---
+++ week2/majority-element.py
@@ -0,0 +1,9 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        count={}
+        for i in nums:
+            count[i] = 1 + count.get(i,0)
+        for i in count:
+            if count.get(i) > len(nums)/2:
+                return i
+            
