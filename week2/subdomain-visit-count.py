d93c79255553cd623ae70b55e2dd2cba386054af
yohannesalex <133195614+yohannesalex@users.noreply.github.com>
2023-12-13 21:20:52 +0300

    Added solution for Subdomain Visit Count

---
+++ week2/subdomain-visit-count.py
@@ -0,0 +1,21 @@
+class Solution:
+    def subdomainVisits(self, cpdomains: List[str]) -> List[str]:
+        hashmap={}
+        res=[]
+        for cp in cpdomains:
+            parts=cp.split(" ")
+            
+            count=parts[0]
+            domain=parts[1]
+            dom=domain.split(".")
+            for i in range(len(dom)):
+                m=".".join(dom[i:])
+                if m not in hashmap:
+                    hashmap[m] = count
+                else:
+                    hashmap[m] = str(int(count)+int(hashmap[m]))
+        for j in hashmap:
+            res.append(hashmap[j]+" "+j)
+        return res
+            
+            
