f8c93998d917048ff16036c34bfb176316fe8cc6
yohannesalex <133195614+yohannesalex@users.noreply.github.com>
2024-04-12 14:52:53 +0300

    Created 'Cyclic Components' by A2SV Hub

---
+++ 12-Apr-2024/Cyclic Components 121407.py
@@ -0,0 +1,44 @@
+# Problem: Cyclic Components - https://codeforces.com/problemset/problem/977/E
+
+from collections import defaultdict
+
+n, edge = map(int, input().split())
+graph = defaultdict(list)
+List = set()
+
+for _ in range(edge):
+    n, m = map(int, input().split())
+    List.add(n)
+    List.add(m)
+    graph[n].append(m)
+    graph[m].append(n)
+visited= set()
+
+def dfs(node, parent):
+   
+    global visited
+    stack=[]
+    stack.append(node)
+    visited.add(node)
+    res = True
+    while stack:
+        node = stack.pop()
+        if len(graph[node]) != 2:
+            res = False
+        for negn in graph[node]:
+            if negn not in visited:
+            
+                stack.append(negn)
+                visited.add(negn)
+                
+                
+    return res
+
+count = 0
+for i in List:
+    
+    if i not in visited:
+        if dfs(i, -1):  
+            count += 1
+
+print(count)
