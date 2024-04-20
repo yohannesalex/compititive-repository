be2bc55c8e265327503e774a1e5df572eafe0ef3
yohannesalex <133195614+yohannesalex@users.noreply.github.com>
2024-04-20 19:30:17 +0300

    Created 'Petya and Strings' by A2SV Hub

---
+++ 20-Apr-2024/Petya and Strings 128792.py
@@ -0,0 +1,17 @@
+# Problem: Petya and Strings - https://codeforces.com/problemset/problem/112/A
+
+
+st1 = input()
+st2 = input()
+flag = True
+for i in range(len(st1)):
+    if ord(st1[i].lower()) > ord(st2[i].lower()):
+        print(1)
+        flag= False
+        break
+    elif ord(st1[i].lower()) < ord(st2[i].lower()):
+        print(-1)
+        flag = False
+        break
+if flag:
+    print(0)
