701a2c78fd947113da0dcd5acde44c9e36fd0d68
yohannesalex <133195614+yohannesalex@users.noreply.github.com>
2024-05-08 11:04:40 +0300

    Created 'Cirno's Perfect Bitmasks Classroom' by A2SV Hub

---
+++ 08-May-2024/Cirno's Perfect Bitmasks Classroom 143097.py
@@ -0,0 +1,29 @@
+# Problem: Cirno's Perfect Bitmasks Classroom - https://codeforces.com/problemset/problem/1688/A
+
+t = int(input())
+
+for i in range(t):
+    s = ""
+    num = int(input())
+    count = 0
+    for i in range(32):
+        if num & 1<< i:
+            count+=1
+    
+
+    for i in range(32):
+        if num & 1<< i:
+            s= "1"+ s
+            break
+        else:
+            s = "0"+ s
+    
+    if count > 1:
+        print(int(s,2))
+    else:
+        cur = int(s,2)
+        for i in range(32):
+            if not num & (1<<i):
+                cur|=(1<<i)
+                break
+        print(int(cur))
