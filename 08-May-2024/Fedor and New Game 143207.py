84b90ee8b91e05f9659b7c3e1ac98a0820da5723
yohannesalex <133195614+yohannesalex@users.noreply.github.com>
2024-05-08 14:28:59 +0300

    Created 'Fedor and New Game' by A2SV Hub

---
+++ 08-May-2024/Fedor and New Game 143207.py
@@ -0,0 +1,22 @@
+# Problem: Fedor and New Game - https://codeforces.com/contest/467/problem/B
+
+n , m , k = list(map(int,input().split()))
+nums = []
+for i in range(m):
+    nums.append(int(input()))
+fedor =int(input())
+check = [0]*m
+for i in range(32):
+    fed_bit = fedor & 1<<i
+    for j  in range(len(nums)):
+        if nums[j] & 1<<i != fed_bit:
+            check[j]+=1
+res=0
+for i in range(m):
+    if check[i] <= k:
+        res+=1
+print(res)
+
+
+
+
