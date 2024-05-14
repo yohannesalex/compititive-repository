1c867fd5eb675679222638b15e94e7662b118a67
yohannesalex <133195614+yohannesalex@users.noreply.github.com>
2024-05-14 18:18:31 +0300

    Created 'A - Non-Identity Permutations' by A2SV Hub

---
+++ A2SV G5 - Contest #16 14-May-2024/A - Non-Identity Permutations 149930.py
@@ -0,0 +1,22 @@
+# Problem: A - Non-Identity Permutations - https://codeforces.com/gym/523525/problem/A
+
+
+t = int(input())
+for i in range(t):
+    n = int(input())
+    nums = []
+    for i in range(1,n+1):
+        nums.append(i)
+    cur = nums.copy()
+    cur.reverse()
+    flag = True
+    if len(nums)%2 == 0:
+        print(*cur)
+        flag = False
+    else:
+        if flag:    
+            cur[len(nums)//2] , cur[len(nums)//2+1] = cur[len(nums)//2+1], cur[len(nums)//2]
+            print(*cur)
+
+
+  
