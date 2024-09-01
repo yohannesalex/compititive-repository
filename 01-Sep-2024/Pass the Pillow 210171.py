49834e48b057acd363a9e2d917275386c6c3907f
yohannesalex <133195614+yohannesalex@users.noreply.github.com>
2024-09-01 21:31:05 +0300

    Created 'Pass the Pillow' by A2SV Hub

---
+++ 01-Sep-2024/Pass the Pillow 210171.py
@@ -0,0 +1,16 @@
+# Problem: Pass the Pillow - https://leetcode.com/problems/pass-the-pillow/
+
+class Solution:
+    def passThePillow(self, n: int, time: int) -> int:
+        stand, flag = 1, 1
+        while time:
+            time -= 1
+            if flag:
+                stand += 1
+                if stand == n:
+                    flag = not flag
+            else:
+                stand -= 1
+                if stand == 1:
+                    flag = not flag
+        return stand
