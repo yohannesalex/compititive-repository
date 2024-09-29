75f27b1c0e263b50e0298f3635166e3dd26f29f9
yohannesalex <133195614+yohannesalex@users.noreply.github.com>
2024-09-29 22:54:11 +0300

    Created 'Maximize Score of Numbers in Ranges' by A2SV Hub

---
+++ 29-Sep-2024/Maximize Score of Numbers in Ranges 217150.py
@@ -0,0 +1,20 @@
+# Problem: Maximize Score of Numbers in Ranges - https://leetcode.com/problems/maximize-score-of-numbers-in-ranges/
+
+class Solution:
+    def maxPossibleScore(self, start: List[int], d: int) -> int:
+        start.sort()
+        
+        def fn(mid): 
+            x = -inf
+            for s in start: 
+                x += mid
+                if x > s+d: return False 
+                x = max(x, s)
+            return True 
+        
+        lo, hi = 0, 2_000_000_000
+        while lo < hi: 
+            mid = lo + hi + 1 >> 1
+            if fn(mid): lo = mid
+            else: hi = mid-1
+        return lo 
