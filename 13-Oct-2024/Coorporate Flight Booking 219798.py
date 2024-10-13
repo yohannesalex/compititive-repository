96945d62d3862a1c8053f1f7809fce90f43cce73
yohannesalex <133195614+yohannesalex@users.noreply.github.com>
2024-10-13 20:12:22 +0300

    Created 'Coorporate Flight Booking' by A2SV Hub

---
+++ 13-Oct-2024/Coorporate Flight Booking 219798.py
@@ -0,0 +1,13 @@
+# Problem: Coorporate Flight Booking - https://leetcode.com/problems/corporate-flight-bookings/
+
+class Solution:
+    def corpFlightBookings(self, bookings: List[List[int]], n: int) -> List[int]:
+        res=[0]*(n+1)
+        for i in bookings:
+            res[i[0]-1]+=i[2]
+            res[i[1]]-=i[2]
+        pre=[res[0]]
+        for i in res[1:]:
+            pre.append(i+pre[-1])
+        pre.pop()
+        return pre
