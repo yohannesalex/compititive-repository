26bf496fbbd33422832908c12873f5ae41c857bd
Yohannes Alemayehu Abdi <133195614+yohannesalex@users.noreply.github.com>
2024-12-27 19:47:22 +0300

    Created 'Best Sightseeing Pair' by A2SV Hub

---
+++ 27-Dec-2024/Best Sightseeing Pair 225027.py
@@ -0,0 +1,17 @@
+# Problem: Best Sightseeing Pair - https://leetcode.com/problems/best-sightseeing-pair/
+
+class Solution:
+    def maxScoreSightseeingPair(self, values: List[int]) -> int:
+        n = len(values)
+        suffixMax = [0] * n
+        suffixMax[n - 1] = values[n - 1] - (n - 1)
+
+        for i in range(n - 2, -1, -1):
+            suffixMax[i] = max(suffixMax[i + 1], values[i] - i)
+
+        maxScore = float('-inf')
+
+        for i in range(n - 1):
+            maxScore = max(maxScore, values[i] + i + suffixMax[i + 1])
+
+        return maxScore
