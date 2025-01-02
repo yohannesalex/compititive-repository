793edc3aee0cd93352b12165c86a3ee5ad816114
Yohannes Alemayehu Abdi <133195614+yohannesalex@users.noreply.github.com>
2025-01-02 11:25:40 +0300

    Created 'Count Vowel Strings in Ranges' by A2SV Hub

---
+++ 02-Jan-2025/Count Vowel Strings in Ranges 225151.py
@@ -0,0 +1,18 @@
+# Problem: Count Vowel Strings in Ranges - https://leetcode.com/problems/count-vowel-strings-in-ranges/
+
+class Solution:
+    def vowelStrings(self, words: List[str], queries: List[List[int]]) -> List[int]:
+        n = len(words)
+        Prefix = [0] * (n + 1)
+        vowels = {'a', 'e', 'i', 'o', 'u'}
+
+        for i in range(n):
+            Prefix[i + 1] = Prefix[i]
+            if words[i][0] in vowels and words[i][-1] in vowels:
+                Prefix[i + 1] += 1
+
+        ANS = []
+        for query in queries:
+            ANS.append(Prefix[query[1] + 1] - Prefix[query[0]])
+
+        return ANS
